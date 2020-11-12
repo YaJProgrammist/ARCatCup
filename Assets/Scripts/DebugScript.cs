2bae8742615b0c28bde87f15e828da658d725b9b
lenskaya <lenskaya.ekaterina@gmail.com>
2020-11-12 16:27:22 +0200

    ArCatCup

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTargetFound()
+    {
+        Debug.Log("Target found");
+    }
+
+    public void OnTargetLost()
+    {
+        Debug.Log("Target lost");
+    }
+}
---
+++ Assets/Scripts/EyesSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject eyes;
+
+    public void ShowEyes()
+    {
+        eyes.SetActive(true);
+    }
+
+    public void HideEyes()
+    {
+        eyes.SetActive(false);
+    }
+}
